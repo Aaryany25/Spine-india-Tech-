dbf5da09be052e104df426b08fa4d78868cc9164
Aryan Yadav <aaryany2505@gmail.com>
2026-01-19 16:49:09 +0530

    fixed the section

--- script.js
+++ script.js
@@ -16,42 +16,45 @@ requestAnimationFrame(raf)
 // register plugin
 gsap.registerPlugin(SplitText);
 
-// split text into words
-const text = new SplitText("#textSplit", {
-  type: "words"
-});
 gsap.fromTo(
   "#aboutRight img",
   {
-    clipPath: "inset(0 0 100% 0)"
+    clipPath: "inset(0 0 100% 0)",
+    // scale:1.1
   },
   {
     clipPath: "inset(0 0 0% 0)",
     ease: "none",
     duration:0.5,
+    scale:1,
     scrollTrigger: {
       trigger: "#aboutRight",
       start: "35% center",
       end: "top 40%",
-    //   scrub: 1.5,
+      scrub: 1.5,
+      pin:true,
     //   markers:true
     }
-  }
+  },"start"
 );
 
-gsap.from(text.words,{
+// split text into words
+const text = new SplitText("#textSplit", {
+  type: "lines"
+});
+gsap.from(text.lines,{
     opacity:0,
     // duration:0.5,
     // delay:2,
-    // stagger:0.2,
+    stagger:0.2,
     filter:blur("10px"),
     // y:20,
     scrollTrigger:{
         trigger:"#AboutSection",
         scrub:0.5,
         // markers:true,
-start:"center center",
-end:"+=1000",
-// pin:true
+start:"45% center",
+end:"+=400",
+pin:true
     }
-})
+},"start")
