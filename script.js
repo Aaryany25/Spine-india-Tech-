989029f82cf8e42b56f14bc1e09806799aae1da5
Aryan Yadav <aaryany2505@gmail.com>
2026-01-19 16:37:07 +0530

    Added Animations

--- script.js
+++ script.js
@@ -10,3 +10,48 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf)
+
+// GSAP
+
+// register plugin
+gsap.registerPlugin(SplitText);
+
+// split text into words
+const text = new SplitText("#textSplit", {
+  type: "words"
+});
+gsap.fromTo(
+  "#aboutRight img",
+  {
+    clipPath: "inset(0 0 100% 0)"
+  },
+  {
+    clipPath: "inset(0 0 0% 0)",
+    ease: "none",
+    duration:0.5,
+    scrollTrigger: {
+      trigger: "#aboutRight",
+      start: "35% center",
+      end: "top 40%",
+    //   scrub: 1.5,
+    //   markers:true
+    }
+  }
+);
+
+gsap.from(text.words,{
+    opacity:0,
+    // duration:0.5,
+    // delay:2,
+    // stagger:0.2,
+    filter:blur("10px"),
+    // y:20,
+    scrollTrigger:{
+        trigger:"#AboutSection",
+        scrub:0.5,
+        // markers:true,
+start:"center center",
+end:"+=1000",
+// pin:true
+    }
+})
